da4e0c00e3d2b80921d395c776ab53481bade684
ChauBui00 <c.bui3@share.epsb.ca>
2022-10-05 11:13:12 -0600

    O

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 // CS10 GRADE CALCULATOR
 
+// Event Listener on the Button
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked(){
+
 // Input
-let S1 = +prompt ("Enter Client-Side 1 Grade: ");
-let SP1 = +prompt ("Enter Structured Programming 1 Grade: ");
-let SP2 = +prompt ("Enter Structured Programming 2 Grade: ");
-let CS2 = +prompt ("Enter Client-Side 2 Grade: ");
-let PA = +prompt ("Enter Project A Grade: ");
+let S1 = +document.getElementById("CS1-in").value;
+let SP1 = +document.getElementById("SP1-in").value;
+let SP2 = +document.getElementById("SP2-in").value;
+let CS2 = +document.getElementById("CS2-in").value;
+let PA = +document.getElementById("PA-in").value;
 
 // Process
 let total = (S1 + SP1 + SP2 + CS2 + PA) / 5; 
 
 // Output
-alert (`Final CS10 Grade: ${total} % `)
+document.getElementById("output")
+
+}
